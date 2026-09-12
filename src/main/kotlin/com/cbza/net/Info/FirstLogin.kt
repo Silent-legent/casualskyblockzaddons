95643f7c6a09ce0134a218af13fd2a6abda40ad7
Silent-legent <174837411+Silent-legent@users.noreply.github.com>
2026-09-12 15:16:42 +0200

    added disc link to 1st login msg
    title

--- src/main/kotlin/com/cbza/net/Info/FirstLogin.kt
+++ src/main/kotlin/com/cbza/net/Info/FirstLogin.kt
@@ -5,7 +5,10 @@ import com.cbza.net.event.EventBus
 import com.cbza.net.event.events.ServerJoinEvent
 import com.cbza.net.feature.mining.general.MiningAbilityTracker.onServerJoin
 import net.minecraft.client.Minecraft
+import net.minecraft.network.chat.ClickEvent
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.HoverEvent
+import net.minecraft.network.chat.Style
 
 object FirstLogin {
     init {
@@ -22,17 +25,36 @@ object FirstLogin {
 
             val client = Minecraft.getInstance()
             client.execute {
-                client.player?.sendSystemMessage(
-                    Component.literal(
-                        "§f════════════════════§7\n" +
-                                "§fWelcome to:§7 §c[§6CasualSkyblockZAddons§c]§7\n" +
-                                "\n" +
-                                "§fUse§7 §e/csz§7, §e/csz hud§7 §fto use our mod.§7\n" +
-                                "§fAnd use§7 §e/csz help§7 §ffor more commands.§7\n" +
-                                "\n" +
-                                "§f════════════════════§7"
+                val discordUrl = "https://discord.gg/UVN99sypv"
+
+                val discordLink = Component.literal(discordUrl)
+                    .withStyle(
+                        Style.EMPTY
+                            .withColor(net.minecraft.ChatFormatting.BLUE)
+                            .withClickEvent(ClickEvent.OpenUrl(java.net.URI.create(discordUrl)))
+                            .withHoverEvent(HoverEvent.ShowText(Component.literal("Click to join the Discord")))
                     )
+
+                val message = Component.literal(
+                    "§f════════════════════§7\n" +
+                            "§fWelcome to:§7 §c[§6CasualSkyblockZAddons§c]§7\n" +
+                            "\n" +
+                            "§fUse§7 §e/csz§7, §e/csz hud§7 §fto use our mod.§7\n" +
+                            "§fAnd use§7 §e/csz help§7 §ffor more commands.§7\n" +
+                            "\n" +
+                            "§fNeed more suport? Join our discord!§7\n"
                 )
+                    .append(discordLink)
+                    .append(
+                        Component.literal(
+                            "§7\n" +
+                                    "\n" +
+                                    "§f════════════════════§7"
+                        )
+                    )
+
+                client.player?.sendSystemMessage(message)
+
                 ModConfig.get().firstTimeUsing = true
                 ModConfig.save()
             }
